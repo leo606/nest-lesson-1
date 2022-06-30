cdb3fc88a850fb34fdd9a5532e7bd809b377afb7
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-06-30 09:27:29 -0300

    custom validations

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -3,6 +3,9 @@ import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
 @Injectable()
 export class ValidationPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
+    console.log(value);
+    console.log(metadata);
+
     return value;
   }
 }
