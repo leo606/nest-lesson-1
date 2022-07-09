43fcb62d6f3efba49b0613e765707d027b9d0773
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-07-08 22:12:12 -0300

    database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,23 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'docker',
+      database: 'postgres',
+      // entities: [__dirname + '../**/*.entity.ts'],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
